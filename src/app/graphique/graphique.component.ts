3953c8dce50b0452bf0aa4408e3abcb14daf3ba7
Bahaaeddine <bahaaeddine.ghezz@gmail.com>
2023-09-20 13:03:15 +0200

    maj

--- src/app/graphique/graphique.component.ts
+++ src/app/graphique/graphique.component.ts
@@ -1,38 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Chart } from 'chart.js';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-graphique',
   templateUrl: './graphique.component.html',
   styleUrls: ['./graphique.component.css']
 })
-export class GraphiqueComponent implements OnInit {
-  graphic: any;
-
-  constructor() { }
-
-  ngOnInit(): void {
-    
-    const data = {
-      labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre' ],
-      datasets: [{
-        data: [1500, 850, 2500, 1200, 8500, 4800],
-        backgroundColor: [
-          'red',
-          'blue',
-          'green',
-          'yellow',
-          'orange',
-          'purple'
-        ]
-      }]
-    };
-
-    this.graphic = new Chart('canvas', {
-      type: 'pie',
-      data: data
-    });
-    
-  }
+export class GraphiqueComponent  {
+  
+ 
 }
-
